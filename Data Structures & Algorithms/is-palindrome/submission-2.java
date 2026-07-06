03633edecad2f33d0da1a5e560d7024282f2fbe6
Saiteja Janyavula <saitejajanyavula@gmail.com>
2026-07-06 22:01:30 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s == null || s.isEmpty() ) {
+            return true;
+        }
+        int p1 = 0;
+        int p2 = s.length() - 1;
+        while (p1 < p2) {
+            while (!Character.isLetterOrDigit(s.charAt(p1))) {
+                p1++;
+
+                if (p1 > p2) {
+                    return true;
+                }
+            }
+            while (!Character.isLetterOrDigit(s.charAt(p2))) {
+                p2--;
+
+                if (p1 > p2) {
+                    return true;
+                }
+            }
+            if (Character.toLowerCase(s.charAt(p1)) != Character.toLowerCase(s.charAt(p2))) {
+                return false;
+            }
+            p1++;
+            p2--;
+        }
+        return true;
+    }
+}
